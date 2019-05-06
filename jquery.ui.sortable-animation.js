99a9a7e0beafecd3912d00d802b4524ef9fa5bb7
Egor Sharapov <egor.sharapov@gmail.com>
2019-05-06 17:32:50 +0300

    lil sortable dependency fix

--- jquery.ui.sortable-animation.js
+++ jquery.ui.sortable-animation.js
@@ -10,7 +10,7 @@
 (function(factory) {
   if (typeof define === "function" && define.amd) {
     // AMD. Register as an anonymous module.
-    define(["jquery"], factory);
+    define(["jquery", "jquery-ui/ui/sortable"], factory);
   } else {
     // Browser globals
     factory(jQuery);
